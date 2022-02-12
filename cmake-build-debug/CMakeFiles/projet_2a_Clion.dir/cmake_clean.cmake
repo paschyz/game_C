bdacdc5a3a4395e37fb5b0790d31ffe8d83f9c41
Nicolas Moissinac <nicolas.moissinac@gmail.com>
2022-02-12 20:55:51 +0100

    Adding Clion files
    Adding Clion files to be able to open project with clion

---
+++ cmake-build-debug/CMakeFiles/projet_2a_Clion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet_2a_Clion.dir/main.c.obj"
+  "libprojet_2a_Clion.dll.a"
+  "projet_2a_Clion.exe"
+  "projet_2a_Clion.exe.manifest"
+  "projet_2a_Clion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projet_2a_Clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/projet_2a_Clion.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for projet_2a_Clion.
+# This may be replaced when dependencies are built.
